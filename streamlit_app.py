558cf00cb3a8bdcdd91c32abb70dc263e8215e69
ogrk <114985726+ogrk@users.noreply.github.com>
2024-11-13 20:55:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Diabetes Prediction App')
 
 st.info('This app uses a machine learning model to predict diabetes')
+
+df = pd.read_csv('https://raw.githubusercontent.com/ogrk/data/refs/heads/main/clean_data.csv')
