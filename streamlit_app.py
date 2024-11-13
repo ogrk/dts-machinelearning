a944713f0c92baf037a27934638cccfaa28c9902
ogrk <114985726+ogrk@users.noreply.github.com>
2024-11-13 20:42:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+st.title('Diabetes Prediction App')
 
-st.write('Hello world!')
+st.info('This app uses a machine learning model to predict diabetes')
